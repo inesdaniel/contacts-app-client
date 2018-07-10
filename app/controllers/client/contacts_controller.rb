c8235aad729fd33eeee3cd4397481127d9e88b8a
Ines Daniel <i.danie8@gmail.com>
2018-07-09 20:51:09 -0500

    add bio column to show in all actions

--- app/controllers/client/contacts_controller.rb
+++ app/controllers/client/contacts_controller.rb
@@ -19,6 +19,7 @@ class Client::ContactsController < ApplicationController
         input_first_name: params[:input_first_name],
         input_middle_name: params[:input_middle_name],
         input_last_name: params[:input_last_name],
+        input_bio: params[:input_bio],
         input_email: params[:input_email],
         input_phone_number: params[:input_phone_number]
       }
@@ -39,6 +40,7 @@ class Client::ContactsController < ApplicationController
         input_first_name: params[:input_first_name],
         input_middle_name: params[:input_middle_name],
         input_last_name: params[:input_last_name],
+        input_bio: params[:input_bio],
         input_email: params[:input_email],
         input_phone_number: params[:input_phone_number]
       } 
